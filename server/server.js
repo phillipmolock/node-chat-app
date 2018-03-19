17c81580fd0906bc9acf0cfce7e6076e16bdfb78
phillipmolock <phillip.molock@outlook.com>
2018-03-19 12:11:57 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -22,6 +22,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
     
+    //socket.emit from Admin text welcome to the chat app
+    
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    //socket.broadcast.emit (everyone but user who joined) from admin text new user joined
+    
+    socket.broadcast.emit('newMessage',{
+       from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+    
 //    socket.emit('newEmail', {
 //        from: 'mike@example.com',
 //        text: 'Hey, whats going on?',
@@ -46,6 +61,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        
+//        socket.broadcast.emit('newMessage',{
+//            from: message.from,
+//            text: message.text,
+//            createdAt: new Date().getTime()
+//        });
     });
     
     socket.on('disconnect',()=>{
