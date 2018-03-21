92ee49487c4b246cc5cbacca4d41658f9138ad2f
phillipmolock <phillip.molock@outlook.com>
2018-03-21 10:45:44 -0500

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ io.on('connection', (socket) => {
         console.log('createMessage', message);
         //io.emit to all connections
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server.');
+        callback();
 //        socket.broadcast.emit('newMessage',{
 //            from: message.from,
 //            text: message.text,
